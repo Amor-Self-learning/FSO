709a999d9681aa1b97922e0447b1719e3110cd50
Abdul Samad <amorzephyr@gmail.com>
2026-08-02 13:03:49 +0500

    Fix: Blog component test ex 5.27

--- part05/bloglist-frontend/src/components/Blog.test.jsx
+++ part05/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,4 @@
 import { screen, render } from '@testing-library/react';
-
-import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 describe('<Blog />', () => {
@@ -9,45 +7,56 @@ describe('<Blog />', () => {
     author: 'Edsger W. Dijkstra',
     url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
     likes: 5,
-    user: '6a69f297e9c77d7a6e9bb826',
+    user: {
+      id: '6a69f297e9c77d7a6e9bb826',
+      username: 'amorzephyr',
+      name: 'Zephyr@CS'
+    }
   };
-  it('Renders only title and author by default.', () => {
-    render(<Blog blog={blog} />);
-    const title = screen.getByText(blog.title);
-    const author = screen.getByText(blog.author);
-    const url = screen.queryByText(blog.url);
-    const likes= screen.queryByText(blog.likes);
-    expect(title).toBeDefined();
-    expect(author).toBeDefined();
-    expect(url).toBeNull();
-    expect(likes).toBeNull();
-  });
+  const users = [
+    {
+      id: '6a69f297e9c77d7a6e9bb826',
+      username: 'amorzephyr',
+      name: 'Abdul Samad'
+    },
+    {
+      id: '6a69f297e9c77d7a6e983826',
+      username: 'harisbaig',
+      name: 'Haris Baig'
+    },
+  ];
 
-  it('Renders full details when view btn is clicked.', async () => {
+  it('Renders information but no buttons to unauthenticated users.', () => {
     render(<Blog blog={blog} />);
-    const user = userEvent.setup();
-    const button = screen.getByText('View');
-    await user.click(button);
     const title = screen.getByText(blog.title);
     const author = screen.getByText(blog.author);
-    const url = screen.queryByText(blog.url);
-    const likes= screen.queryByText(blog.likes);
+    const url = screen.getByText(blog.url);
+    const likes= screen.getByText(blog.likes);
+    expect(screen.queryByRole('button', { name: /like/i })).toBeNull();
+    expect(screen.queryByRole('button', { name: /delete/i })).toBeNull();
     expect(title).toBeDefined();
     expect(author).toBeDefined();
     expect(url).toBeDefined();
     expect(likes).toBeDefined();
   });
 
-  it('calls handleLikeClick twice when like button is clicked twice', async () => {
-    const setMessage = (message) => console.log(message);
-    const handleMock = vi.fn();
-    render(<Blog blog={blog} setMessage={setMessage} handleLikeClick={handleMock}/>);
-    const user = userEvent.setup();
-    const viewButton = screen.getByRole('button', { name: 'View' });
-    await user.click(viewButton);
-    const likeButton = screen.getByRole('button', { name: 'Like' });
-    await user.click(likeButton);
-    await user.click(likeButton);
-    expect(handleMock.mock.calls).toHaveLength(2);
+  it('Show only like button to authenticated users who is not the creator of the blog.', () => {
+    render(<Blog blog={blog} user={users[1]}/>);
+    expect(screen.getByText(blog.title)).toBeDefined();
+    expect(screen.getByText(blog.author)).toBeDefined();
+    expect(screen.getByText(blog.url)).toBeDefined();
+    expect(screen.getByText(blog.likes)).toBeDefined();
+    expect(screen.getByRole('button', { name: /like/i })).toBeDefined();
+    expect(screen.queryByRole('button', { name: /delete/i })).toBeNull();
+  });
+
+  it('Show both like and delete button to authenticated user who is the creator of the blog.', ()  => {
+    render(<Blog blog={blog} user={users[0]}/>);
+    expect(screen.getByText(blog.title)).toBeDefined();
+    expect(screen.getByText(blog.author)).toBeDefined();
+    expect(screen.getByText(blog.url)).toBeDefined();
+    expect(screen.getByText(blog.likes)).toBeDefined();
+    expect(screen.getByRole('button', { name: /like/i })).toBeDefined();
+    expect(screen.getByRole('button', { name: /delete/i })).toBeDefined();
   });
 });
